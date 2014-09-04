e4f7bb44bf38d4ad7b88125b55fc7c3a0463d4f4
Leigh Zhu <i@zhuli.me>
2014-09-04 17:41:42 +0800

    feat: support parse html files

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+var path = require('path');
+
 var through = require('through2');
 var gutil = require('gulp-util');
-var path = require('path');
+var select = require('html-select');
+var tokenize = require('html-tokenize');
 
 function replace(config) {
     var cssre = /\.css$/;
     var jsre = /\.js$/;
+    var html = /\.html/;
 
     var files = {
         css: [],
-        js: []
+        js: [],
+        html: []
     };
 
 
@@ -17,6 +22,8 @@ function replace(config) {
             files.css.push(file);
         } else if(jsre.test(file.path)) {
             files.js.push(file);
+        } else if(html.test(file.path)) {
+            files.html.push(file);
         }
         cb();
     }, function(cb) {
@@ -24,8 +31,9 @@ function replace(config) {
         Object.keys(files).forEach(function(type) {
             if(type === 'css') parser(/url\((.*)\)/g, files.css, ctx);
             if(type === 'js') parser(/src\s*=\s*['"](.*)['"]/g, files.js, ctx);
+            if(type === 'html') htmlParser(files.html, ctx);
         });
-        cb();
+        //cb();
     });
 
     function parser(re, files, ctx) {
@@ -62,6 +70,49 @@ function replace(config) {
             });
 
         });
+    }
+
+    function htmlParser(files, ctx) {
+        
+        files.forEach(function(file) {
+            var fileBuf = [];
+
+            var stream = through();
+            stream.write(file.contents.toString());
+            stream.pipe(tokenize()).pipe(select('img', function(e) {
+                var _src = e.getAttribute('src');
+                var url = ''
+
+                if(_src.indexOf('/') !== 0) {
+                    var tmp = path.resolve(filePath, _src);
+                    tmpPaths = tmp.split(path.sep);
+                    tmpPaths = tmpPaths.slice(tmpPaths.indexOf(config.base) + 1);
+                    url = config.cdn + '/' + path.join.apply(null, tmpPaths);
+                } else {
+                    url = config.cdn + _src;
+                }
+
+                e.setAttribute('src', url);
+                var tr = through.obj(function(row, enc, next) {
+                    this.push([row[0], row[1]]);
+                    next();
+                });
+                tr.pipe(e.createStream()).pipe(tr)
+            })).pipe(through.obj(function(row, buf, next) {
+                this.push(row[1]);
+                next();
+            })).on('data', function(data) {
+                if(!Buffer.isBuffer(data)) data = new Buffer(data);
+                fileBuf.push(data);
+            }).on('end', function() {
+                file.contents = Buffer.concat(fileBuf);
+                ctx.push(file);
+            })
+
+            stream.end();
+
+            
+        })
     }
 
 }
